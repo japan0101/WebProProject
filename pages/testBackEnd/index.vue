68a50f3010a5cc8dd0ec4fc4e40b2194a3ebc318
bess11234 <bess11234@hotmail.com>
2024-02-21 20:57:00 +0700

    responsive order page

--- pages/testBackEnd/index.vue
+++ pages/testBackEnd/index.vue
@@ -1,40 +1,24 @@
-<script setup>
-import { ref, reactive } from 'vue';
-const count = ref(0)
-</script>
-
 <template>
-  <div>
-    <h1>Data from PHP:</h1>
-    <p>{{ jsonData.name }}</p>
-  </div>
+  <div class="text">This text has the color defined by the theme.</div>
 </template>
 
+<script setup>
+import { ref } from 'vue';
+import { onMounted } from 'vue';
 
-<script>
-import axios from 'axios';
-export default {
-  data() {
-    return {
-      jsonData: []  // Initialize an empty array to store the fetched JSON data
-    };
-  },
-  created() {
-    // Fetch data from PHP file when the component is created
-    this.fetchData();
-  },
-  methods: {
-    async fetchData() {
-      try {
-        // Make an HTTP GET request to the PHP file
-        const response = await axios.get('/api/backend/test.php');
-
-        // Assign the fetched JSON data to the component's data property
-        this.jsonData = response.data;
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    }
-  }
+const theme = {
+  color:'red'
 }
+
+onMounted(() => {
+  if (window.outerWidth < 1100) {
+    theme.color = 'blue';
+  }
+});
 </script>
+
+<style scoped>
+.text {
+  color: v-bind('theme.color');
+}
+</style>
