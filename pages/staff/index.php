24f7f45b5dea338b8a115c068bee45b7034ed151
japan0101 <japan.pon11@gmail.com>
2024-02-24 22:02:37 +0700

    staff login and table table gen gen

--- pages/staff/index.php
+++ pages/staff/index.php
@@ -1,26 +1,14 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <?php session_start(); ?>
+
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 
   <?php include($_SERVER['DOCUMENT_ROOT'] . "/asset/script/tailwind.php") ?>
-  <script>
-    import {
-      Modal,
-      Ripple,
-      initTE,
-    } from "tw-elements";
-
-    initTE({
-      Modal,
-      Ripple
-    });
-    // Initialization for ES Users
-  </script>
 </head>
 
 <body>
@@ -28,24 +16,98 @@
 
   $isAuth = isset($_SESSION['userID']);
   if (!$isAuth) { ?>
-    <div class="relative mb-5" data-te-input-wrapper-init>
-      <input type="text" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" name="credential" id="loginEmail" aria-describedby="emailHelp" placeholder="Enter email" />
-      <label for="loginEmail" class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">หมายเลขโทรศัพท์</label>
-    </div>
+    <section class="h-screen">
+        <div class="h-full">
+            <!-- Left column container with background-->
+            <div class="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
+                <div class="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
+                    <img src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" class="w-full" alt="Sample image" />
+                </div>
 
-    <!--Password input-->
-    <div class="relative mb-2" data-te-input-wrapper-init>
-      <input type="password" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" name="passwd" id="loginPassword" placeholder="Password" />
-      <label for="loginPassword" class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">รหัสผ่าน</label>
-    </div>
+                <!-- Right column container -->
+                <div class="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
 
-    <!--Checkbox-->
-    <div class="mb-6 block min-h-[1.5rem] pl-[1.5rem]">
-      <input class="relative float-left -ml-[1.5rem] mr-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[""] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[""] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[""] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]" type="checkbox" value="1" id="token" name="token" />
-      <label class="inline-block pl-[0.15rem] hover:cursor-pointer" for="checkboxDefault">
-        จดจำบัญชีนี้ไว้
-      </label>
-    </div>';
+                    <form method="post" action="./../backend/account/read_user.php">
+
+                        <!-- Email input -->
+                        <div class="relative mb-6 flex-1" id="credentialInput" data-te-input-wrapper-init>
+                            <input type="text" name="credential" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" id="credential" placeholder="อีเมลหรือหมายเลขโทรศัพท์" title='ต้องเป็นอีเมล หรือ หมายเลขโทรศัพท์' required />
+                            <label for="credential" class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">อีเมลหรือหมายเลขโทรศัพท์
+                            </label>
+                        </div>
+
+                        <!-- Password input -->
+                        <div class="relative mb-6" id="passwdInput" data-te-input-wrapper-init>
+                            <input type="password" name="passwd" pattern=".{8,}" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" id="password" placeholder="รหัสผ่าน" required title='ต้องมีความยาว 8 ตัวอักษรขึ้นไป' />
+                            <label for="password" class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">รหัสผ่าน
+                            </label>
+                        </div>
+
+                        <div class="mb-6 flex items-center justify-between">
+                            <!-- Remember me checkbox -->
+                            <div class="mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]">
+                                <input class="relative float-left -ml-[1.5rem] mr-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]" type="checkbox" value="1" id="remember" name="remember" />
+                                <label class="inline-block pl-[0.15rem] hover:cursor-pointer" for="remember">
+                                    จดจำบัญชีนี้ไว้
+                                </label>
+                            </div>
+                        </div>
+
+                        <!-- Login button -->
+                        <div class="text-center lg:text-left">
+                            <button type="submit" class="inline-block rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]" data-te-ripple-init data-te-ripple-color="light">
+                                เข้าสู่ระบบ
+                            </button>
+
+                            <!-- Register link -->
+                            <p class="mb-0 mt-2 pt-1 text-sm font-semibold">
+                                ไม่มีบัญชี?
+                                <a href="register.php" class="text-danger transition duration-150 ease-in-out hover:text-danger-600 focus:text-danger-600 active:text-danger-700">สมัครสมาชิก</a>
+                            </p>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="/asset/script/sweetalert.js"></script>
+    <script>
+        if (<?php echo $_SESSION['result']['result'] == 0 && $_SESSION['result']['type'] == 'login';?>) {
+            Toast.fire({
+                icon: "success",
+                title: "<?php echo $_SESSION['result']['message'] ?>",
+            });
+            <?php unset($_SESSION['result']); ?>
+        } else if (<?php echo $_SESSION['result']['result'] == 0 && $_SESSION['result']['type'] == 'login';?>) {
+            console.log(<?php echo $_SESSION['result']['result'] == 0 && $_SESSION['result']['type'] == 'login';?>)
+            Toast.fire({
+                icon: "error",
+                title: "<?php echo $_SESSION['result']['message'] ?>",
+            });
+            <?php unset($_SESSION['result']); ?>
+        }
+    </script>
+
+
+    <?php include($_SERVER['DOCUMENT_ROOT'] . "/asset/script/tw_element.php") ?>
+    <script>
+        // Initialization for ES Users
+        import {
+            Animate,
+            Input,
+            Ripple,
+            initTE,
+        } from "tw-elements";
+
+        initTE({
+            Animate,
+            Input,
+            Ripple
+        });
+    </script>;
   <?php } ?>
 
   <?php if ($isAuth && ($_SESSION['role'] == "STAFF" || $_SESSION['role'] == "MANAGER")) { ?>
@@ -58,7 +120,6 @@
         <a href="#tabs-profile" class="my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400" data-te-toggle="pill" data-te-target="#tabs-profile" role="tab" aria-controls="tabs-profile" aria-selected="false">คิวการรอ</a>
       </li>
     </ul>
-
     <!--Tabs content-->
     <div class="mb-6">
       <div class="place-content-center hidden opacity-100 transition-opacity duration-150 ease-linear data-[te-tab-active]:block" id="tabs-home" role="tabpanel" aria-labelledby="tabs-home-tab" data-te-tab-active>
@@ -76,16 +137,18 @@
                     </tr>
                   </thead>
                   <tbody>
+                    <?php for ($i=0; $i < 5; $i++) { ?>
                     <tr class="border-b dark:border-neutral-500">
                       <td class="whitespace-nowrap px-6 py-4 font-medium">1</td>
                       <td class="whitespace-nowrap px-6 py-4">Mark</td>
                       <td class="whitespace-nowrap px-6 py-4">Otto</td>
                       <td class="whitespace-nowrap px-6 py-4">@mdo</td>
-                      <td class="whitespace-nowrap px-6 py-4 text-center">
-                        <button class="my-1 px-3 py-1 rounded-lg bg-primary text-white">คิดเงิน</button><br>
-                        <button class="my-1 px-3 py-1 rounded-lg bg-primary text-white">สร้างรหัสเมนู</button>
+                      <td class="whitespace-nowrap px-6 py-4 text-center grid grid-cols-2 gap-4">
+                        <button data-te-ripple-init data-te-ripple-color="light" class="px-3 py-2 rounded-lg bg-primary text-white">คิดเงิน</button>
+                        <button data-te-ripple-init data-te-ripple-color="light" class="px-3 py-2 rounded-lg bg-primary text-white">สร้างรหัสเมนู</button>
                       </td>
                     </tr>
+                    <?php } ?>
                   </tbody>
                 </table>
               </div>
@@ -96,21 +159,19 @@
       <div class="hidden opacity-0 transition-opacity duration-150 ease-linear data-[te-tab-active]:block" id="tabs-profile" role="tabpanel" aria-labelledby="tabs-profile-tab">
         insert here
       </div>
-      <div class="place-content-center hidden opacity-0 transition-opacity duration-150 ease-linear data-[te-tab-active]:block" id="tabs-messages" role="tabpanel" aria-labelledby="tabs-profile-tab">
-        Tab 3 content
-      </div>
-      <div class="hidden opacity-0 transition-opacity duration-150 ease-linear data-[te-tab-active]:block" id="tabs-contact" role="tabpanel" aria-labelledby="tabs-contact-tab">
-        Tab 4 content
-      </div>';
-    <?php } ?>
+    <?php }?>
     <?php include($_SERVER['DOCUMENT_ROOT'] . "/asset/script/tw_element.php") ?>
     <script>
       import {
+        Modal,
+        Ripple,
         Tab,
         initTE,
       } from "tw-elements";
 
       initTE({
+        Modal,
+        Ripple,
         Tab
       });
     </script>
