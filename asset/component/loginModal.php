a49088c3e65931deb63d09bb34afc2cb56def9d5
bess11234 <bess11234@hotmail.com>
2024-02-25 15:26:41 +0700

    fix Toast fire

--- asset/component/loginModal.php
+++ asset/component/loginModal.php
@@ -20,7 +20,7 @@
         <form action="/backend/account/read_user.php" method="post">
           <!--E-mail input-->
           <div class="relative mb-5" data-te-input-wrapper-init>
-            <input type="text" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" name="credential" id="loginEmail" aria-describedby="emailHelp" placeholder="Enter email" title='ต้องเป็นอีเมล หรือ หมายเลขโทรศัพท์' required/>
+            <input type="text" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" name="credential" id="loginEmail" aria-describedby="emailHelp" placeholder="Enter email" title='ต้องเป็นอีเมล หรือ หมายเลขโทรศัพท์' required />
             <label for="loginEmail" class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">อีเมลหรือหมายเลขโทรศัพท์</label>
           </div>
 
@@ -52,20 +52,38 @@
 <script src="/asset/script/sweetalert.js"></script>
 <?php if (isset($_SESSION['result'])) { ?>
   <script>
+    <?php $fire = false; ?>
     <?php if (($_SESSION['result']['result'] == 1) && ($_SESSION['result']['type'] == "login")) { ?>
       Toast.fire({
         icon: "success",
         title: "<?php echo $_SESSION['result']['message']; ?>",
       });
-    <?php unset($_SESSION['result']);
-    } ?>
+      <?php $fire = true; ?>
 
-    <?php if (($_SESSION['result']['result'] == 0) && ($_SESSION['result']['type'] == "login")) { ?>
+    <?php } else if (($_SESSION['result']['result'] == 0) && ($_SESSION['result']['type'] == "login")) { ?>
       Toast.fire({
         icon: "error",
         title: "<?php echo $_SESSION['result']['message']; ?>",
       });
-    <?php unset($_SESSION['result']);
+    <?php $fire = true;
     } ?>
+
+
+    <?php if (($_SESSION['result']['result'] == 1) && ($_SESSION['result']['type'] == "register")) { ?>
+      Toast.fire({
+        icon: "success",
+        title: "<?php echo $_SESSION['result']['message']; ?>",
+      });
+      <?php $fire = true; ?>
+
+    <?php } else if (($_SESSION['result']['result'] == 0) && ($_SESSION['result']['type'] == "register")) { ?>
+      Toast.fire({
+        icon: "error",
+        title: "<?php echo $_SESSION['result']['message']; ?>",
+      });
+    <?php $fire = true;
+    } ?>
+
+    <?php if ($fire) unset($_SESSION['result']) ?>
   </script>
 <?php } ?>
