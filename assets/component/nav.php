f9296dc3a3e322e2042ae8242863d98f25ec9b0e
tae13152 <65070089@kmitl.ac.th>
2024-02-26 19:49:48 +0700

    Rename from "css" to "stylesheets" and Do Refactor

--- assets/component/nav.php
+++ assets/component/nav.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="/assets/css/navbar.css">
-<link rel="stylesheet" href="/assets/css/global.css">
+<link rel="stylesheet" href="/assets/stylesheets/navbar.css">
+<link rel="stylesheet" href="/assets/stylesheets/global.css">
 <header>
     <nav id="navbar" style="position: fixed !important"
          class="fixed top-0 left-0 w-full z-50 flex-no-wrap relative flex items-center justify-between bg-[#FBFBFB] py-2 shadow-md shadow-black/5 dark:bg-neutral-600 dark:shadow-black/10 lg:flex-wrap lg:justify-start lg:py-4 z-40 shadow-lg">
