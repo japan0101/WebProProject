5ed0ce2113fa95dcc657ea6cbc9eb369881088e3
bess11234 <bess11234@hotmail.com>
2024-02-29 10:25:07 +0700

    update usecase 7 complete

--- backend/database/manager.php
+++ backend/database/manager.php
@@ -9,78 +9,65 @@ $redirect = "Location: ";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST" && $_SESSION['role'] == "MANAGER") {
     switch ($_POST["case"]) {
-        case 'insertTable':
-        {
-            // ต้องการ capacity
+        case 'insertTable': {
+                // ต้องการ capacity
 
-            $database->insert("tables", array("capacity" => $_POST['capacity']));
-            if ($database->getResult()['result'])
-                $database->customResult(message: "ทำการสร้าง โต๊ะ เสร็จสิ้น");
-
-            break;
-        }
-        case 'change_status':
-        {
-            // ต้องการ ID
-
-            $database->custom("SELECT status FROM users WHERE userID={$_POST['ID']}");
-
-            if ($database->getResult()['payload'][0]->status == "ACTIVE") {
-
-                $database->update("users", array("status" => 2), "userID={$_POST['ID']}");
+                $database->insert("tables", array("capacity" => $_POST['capacity']));
                 if ($database->getResult()['result'])
-                    $database->customResult(message: "ระงับบัญชีผู้ใช้เรียบร้อย");
-            } else {
+                    $database->customResult(message: "ทำการสร้าง โต๊ะ เสร็จสิ้น");
 
-                $database->update("users", array("status" => 1), "userID={$_POST['ID']}");
-                if ($database->getResult()['result'])
-                    $database->customResult(message: "เปิดใช้งานบัญชีผู้ใช้เรียบร้อย");
+                break;
             }
-            usleep(0.5 * 1000000);
-            break;
-        }
-        case 'change_passwd':
-        {
-            // ต้องการ ID, passwd (ยังไม่ได้ทำ อาจให้ Manager INPUT รหัสผ่านที่ต้องการให้เปลี่ยนเลย)
+        case 'change_status': {
+                // ต้องการ ID
 
-            $password = "12345678";
-            $hashedPassword = password_hash("12345678", PASSWORD_BCRYPT);
-            $database->update("users", array("passwd" => $hashedPassword), "userID={$_POST['ID']}");
-            if ($database->getResult()['result'])
-                $database->customResult(message: "เปลี่ยนรหัสผ่านเสร็จสิ้น รหัสผ่าน: " . $password);
+                $database->custom("SELECT status FROM users WHERE userID={$_POST['ID']}");
 
-            break;
-        }
-        case 'create_category':
-        {
-            // name
+                if ($database->getResult()['payload'][0]->status == "ACTIVE") {
 
-            $database->insert("menu_category", array('name' => $_POST['name']));
-            if ($database->getResult()['result'])
-                $database->customResult(message: "เพิ่มประเภทเมนูเสร็จสิ้น");
-            break;
-        }
-        case 'create_menu':
-        {
-            // name, category, price, description(Optional), image(Optional)
+                    $database->update("users", array("status" => 2), "userID={$_POST['ID']}");
+                    if ($database->getResult()['result'])
+                        $database->customResult(message: "ระงับบัญชีผู้ใช้เรียบร้อย");
+                } else {
 
-            $insert = array('menuName' => $_POST['name'], 'price' => $_POST['price'], 'categoryID' => $_POST['category']);
-            if (isset($_POST['description']))
-                $insert['description'] = $_POST['description'];
-            if (isset($_POST['image']))
-                $insert['image'] = $_POST['image'];
+                    $database->update("users", array("status" => 1), "userID={$_POST['ID']}");
+                    if ($database->getResult()['result'])
+                        $database->customResult(message: "เปิดใช้งานบัญชีผู้ใช้เรียบร้อย");
+                }
+                usleep(0.5 * 1000000);
+                break;
+            }
+        case 'change_passwd': {
+                // ต้องการ ID, passwd (ยังไม่ได้ทำ อาจให้ Manager INPUT รหัสผ่านที่ต้องการให้เปลี่ยนเลย)
 
-            $database->insert("menus", $insert);
-            if ($database->getResult()['result'])
-                $database->customResult(message: "เพิ่มเมนูเสร็จสิ้น");
-            break;
-        }
-        default:
-        {
-            $database->customResult(result: 0, message: "ไม่ได้ใส่สิ่งที่ต้องการ");
-        }
+                $password = "12345678";
+                $hashedPassword = password_hash("12345678", PASSWORD_BCRYPT);
+                $database->update("users", array("passwd" => $hashedPassword), "userID={$_POST['ID']}");
+                if ($database->getResult()['result'])
+                    $database->customResult(message: "เปลี่ยนรหัสผ่านเสร็จสิ้น รหัสผ่าน: " . $password);
+
+                break;
+            }
+        case 'create_category': {
+                // name
+
+                $database->insert("menu_category", array('name' => $_POST['name']));
+                if ($database->getResult()['result'])
+                    $database->customResult(message: "เพิ่มประเภทเมนูเสร็จสิ้น");
+                break;
+            }
+        case 'create_menu': {
+                // name, category, price, description(Optional), image(Optional)
+
+                $insert = array('menuName' => $_POST['name'], 'price' => $_POST['price'], 'categoryID' => $_POST['category']);
+                if (isset($_POST['description']))
+                    $insert['description'] = $_POST['description'];
+                if (isset($_POST['image']))
+                    $insert['image'] = $_POST['image'];
+
                 $database->insert("menus", $insert);
-                if ($database->getResult()['result']) $database->customResult(message: "เพิ่มเมนูเสร็จสิ้น");
+                if ($database->getResult()['result'])
+                    $database->customResult(message: "เพิ่มเมนูเสร็จสิ้น");
                 break;
             }
         case 'modify_menu': {
@@ -109,25 +96,22 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && $_SESSION['role'] == "MANAGER") {
     $database->customResult(type: $_POST['case']);
 } else if ($_SERVER['REQUEST_METHOD'] == "GET" && $_SESSION['role'] == "MANAGER") {
     switch ($_GET['case']) {
-        case 'alluser':
-        {
-            $database->custom("SELECT phoneNumber, memberName, email, status, points, role, userID FROM users WHERE NOT userID={$_SESSION['userID']} ORDER BY role");
-            echo json_encode($database->getResult()['payload']);
-            break;
-        }
+        case 'alluser': {
+                $database->custom("SELECT phoneNumber, memberName, email, status, points, role, userID FROM users WHERE NOT userID={$_SESSION['userID']} ORDER BY role");
+                echo json_encode($database->getResult()['payload']);
+                break;
+            }
 
-        case 'menu_category':
-        {
-            $database->custom("SELECT categoryID, name FROM menu_category");
-            echo json_encode($database->getResult()['payload']);
-            break;
-        }
+        case 'menu_category': {
+                $database->custom("SELECT categoryID, name FROM menu_category");
+                echo json_encode($database->getResult()['payload']);
+                break;
+            }
 
 
-        default:
-        {
-            $database->customResult(result: 0, message: "ไม่ได้ใส่สิ่งที่ต้องการ");
-        }
+        default: {
+                $database->customResult(result: 0, message: "ไม่ได้ใส่สิ่งที่ต้องการ");
+            }
     }
     return;
 } else {
