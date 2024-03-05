1a50f44bd0555db453843be9de6c7c5f6f5d3e52
bess11234 <bess11234@hotmail.com>
2024-03-05 22:36:04 +0700

    setcookie change path

--- backend/checkTable.php
+++ backend/checkTable.php
@@ -6,18 +6,18 @@ include './connectDatabase.php';
 
 $database->custom("SELECT tableID FROM tables WHERE code='{$_SESSION['tablecode']}'");
 if ($database->getResult()['result']) {
-    setcookie("tableID", $database->getResult()['payload'][0]->tableID, time() + 60 * 60 * 6, '/pages/order');
+    setcookie("tableID", $database->getResult()['payload'][0]->tableID, time() + 60 * 60 * 6, './../pages/order');
     setcookie("tablecode", $_SESSION['tablecode'], time() + 60 * 60 * 6, '/pages/order');
 
-    setcookie("tableID", $database->getResult()['payload'][0]->tableID, time() + 60 * 60 * 6, '/backend/database/customer.php');
-    setcookie("tablecode", $_SESSION['tablecode'], time() + 60 * 60 * 6, '/backend/database/customer.php');
+    setcookie("tableID", $database->getResult()['payload'][0]->tableID, time() + 60 * 60 * 6, './../backend/database/customer.php');
+    setcookie("tablecode", $_SESSION['tablecode'], time() + 60 * 60 * 6, './../backend/database/customer.php');
 
 } else {
-    setcookie("tableID", $database->getResult()['payload'][0]->tableID, time() - 60 * 60 * 4, '/pages/order');
-    setcookie("tablecode", $_SESSION['tablecode'], time() - 60 * 60 * 4, '/pages/order');
+    setcookie("tableID", $database->getResult()['payload'][0]->tableID, time() - 60 * 60 * 4, './../pages/order');
+    setcookie("tablecode", $_SESSION['tablecode'], time() - 60 * 60 * 4, './../pages/order');
 
-    setcookie("tableID", $database->getResult()['payload'][0]->tableID, time() - 60 * 60 * 6, '/backend/database/customer.php');
-    setcookie("tablecode", $_SESSION['tablecode'], time() - 60 * 60 * 4, '/backend/database/customer.php');
+    setcookie("tableID", $database->getResult()['payload'][0]->tableID, time() - 60 * 60 * 6, './../backend/database/customer.php');
+    setcookie("tablecode", $_SESSION['tablecode'], time() - 60 * 60 * 4, './../backend/database/customer.php');
     unset($_SESSION['tablecode']);
 }
 
