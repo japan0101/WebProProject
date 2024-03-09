2b158cbbf22455f4dae8f5f0c82d531395e07b6c
bess11234 <bess11234@hotmail.com>
2024-03-09 11:06:02 +0700

    fix pages/cooking จาก เสิร์ฟ เป็น เสริฟ

--- pages/cooking/index.php
+++ pages/cooking/index.php
@@ -87,7 +87,7 @@ if ($isAuth && ($_SESSION['role'] == "STAFF" || $_SESSION['role'] == "MANAGER"))
             <a href="#tabs-profile"
                class="my-2 block border-x-0 border-t-0 px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:bg-neutral-100 focus:isolate data-[te-nav-active]:border-b-2 border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
                data-te-toggle="pill" data-te-target="#tabs-profile" role="tab" aria-controls="tabs-profile"
-               aria-selected="false">พนักงานเสิร์ฟ</a>
+               aria-selected="false">พนักงานเสริฟ</a>
         </li>
     </ul>
 
